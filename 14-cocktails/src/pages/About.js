d08687b40bfbc894f1974cfb85f38716a850d037
Rodrigo Braga <rodrigo.cbraga@hotmail.com>
2021-04-28 10:45:06 -0300

    Add Projeto 14- Cocktails com api

---
+++ 14-cocktails/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eros urna, iaculis sit amet sodales sed, laoreet sit amet quam. Sed porta ligula est, ac euismod mauris tempor sed. Praesent et nibh nec dui commodo rutrum quis ut diam. Proin eu pretium mauris. Curabitur sed bibendum mi. Mauris id urna id nisl fermentum finibus. Maecenas nec placerat enim. Nam sit amet commodo erat.
+      </p>
+    </section>
+  )
+}
+
+export default About
